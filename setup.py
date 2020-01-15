78bdb054cfbb85dc1748bfb2a00fb71729f10d80
Abirafdi Raditya Putra <abirafdiraditya@gmail.com>
2020-01-15 19:49:35 +0700

    Upgrade oidc-validators in setup.py

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
         'google-cloud-tasks==1.3.0',
         'cryptography==2.7',
         'PyJWT==1.7.1',
-        'oidc-validators==0.5.2'
+        'oidc-validators==0.5.3'
     ],
     zip_safe=False,
     python_requires=">=3.7",
